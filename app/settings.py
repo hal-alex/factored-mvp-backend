fd680f3c7981dd494422bd1ceee0657fd430adb8
Alex <1995aha@gmail.com>
2023-01-21 19:42:52 +0000

    adding S3 support

--- app/settings.py
+++ app/settings.py
@@ -148,7 +148,7 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_USER = 'apikey' 
 EMAIL_HOST_PASSWORD = env('SENDGRID_API_KEY')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
