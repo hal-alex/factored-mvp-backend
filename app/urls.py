553f641a0f6ef268493d35568fbae868b78b3ada
Alex <1995aha@gmail.com>
2023-02-06 16:48:21 +0000

    added 2FA to admin

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django_otp.admin import OTPAdminSite
+  
+admin.site.__class__ = OTPAdminSite
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
